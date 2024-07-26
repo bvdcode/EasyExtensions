5bf7a7575c191f18744401878fdb3781597b8fb9
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-07-26 02:16:06 -0700

    Add Authorization project and CORS/JWT extensions
    Updated `VisualStudioVersion` in `EasyExtensions.sln` to `17.10.35027.167`.
    
    Added new project `EasyExtensions.Authorization` targeting `.NET 8.0` with nullable reference types enabled and various metadata specified. Included project and package references.
    
    Introduced new extension methods in `ServiceCollectionExtensions.cs` for CORS policy configuration, JWT authentication setup, and conditional authorization handling based on the environment.
    
    Created `AllowAnonymousAuthorizationHandler.cs` to define a handler that bypasses authorization in development environments.

---
+++ Sources/EasyExtensions.Authorization/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authorization;
+using EasyExtensions.Authorization.Handlers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace EasyExtensions.Authorization.Extensions
+{
+    /// <summary>
+    /// <see cref="IServiceCollection"/> extensions.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds CORS policy with origins.
+        /// </summary>
+        /// <param name="services"> <see cref="IServiceCollection"/> instance. </param>
+        /// <param name="policyName"> Name of the policy. </param>
+        /// <param name="origins"> Origins to add to the policy. </param>
+        /// <returns></returns>
+        public static IServiceCollection AddCorsWithOrigins(this IServiceCollection services, string policyName, params string[] origins)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: policyName,
+                    policy =>
+                    {
+                        policy.WithOrigins(origins)
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                        .AllowAnyMethod()
+                        .WithExposedHeaders("*");
+                    });
+            });
+            return services;
+        }
+
+        /// <summary>
+        /// Adds JWT authentication from JwtSettings section or Jwt[Key] configuration values.
+        /// </summary>
+        /// <param name="services"> <see cref="IServiceCollection"/> instance. </param>
+        /// <param name="configuration"> Configuration from which to get JWT settings. </param>
+        /// <returns> Current <see cref="IServiceCollection"/> instance. </returns>
+        /// <exception cref="KeyNotFoundException"> When JwtSettings section is not set. </exception>
+        public static IServiceCollection AddJwt(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+
+            string secret = (!jwtSettings.Exists() ? configuration["JwtKey"] : jwtSettings["Key"])
+                ?? throw new KeyNotFoundException("JwtSettings.Key or JwtKey is not set");
+
+            string issuer = (!jwtSettings.Exists() ? configuration["JwtIssuer"] : jwtSettings["Issuer"])
+                ?? throw new KeyNotFoundException("JwtSettings.Issuer or JwtIssuer is not set");
+
+            string audience = (!jwtSettings.Exists() ? configuration["JwtAudience"] : jwtSettings["Audience"]) 
+                ?? throw new KeyNotFoundException("JwtSettings.Audience or JwtAudience is not set");
+
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer(options =>
+                {
+                    byte[] bytes = System.Text.Encoding.UTF8.GetBytes(secret);
+                    var key = new SymmetricSecurityKey(bytes);
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = issuer,
+                        ValidateAudience = true,
+                        ValidAudience = audience,
+                        ValidateLifetime = true,
+                        IssuerSigningKey = key,
+                        ValidateIssuerSigningKey = true,
+                        ClockSkew = TimeSpan.FromMinutes(5)
+                    };
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            var accessToken = context.Request.Query["access_token"].ToString();
+                            var path = context.HttpContext.Request.Path;
+                            if (!string.IsNullOrEmpty(accessToken))
+                            {
+                                context.Token = accessToken;
+                            }
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+            return services;
+        }
+
+        /// <summary>
+        /// Ignore <see cref="AuthorizeAttribute"/> when application is on development environment.
+        /// </summary>
+        /// <returns> Current <see cref="IServiceCollection"/> instance. </returns>
+        public static IServiceCollection AllowAnonymousOnDevelopment(this IServiceCollection services)
+        {
+            string currentEnvironment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? string.Empty;
+            if (currentEnvironment == "Development")
+            {
+                var descriptor = new ServiceDescriptor(typeof(IAuthorizationHandler), typeof(AllowAnonymousAuthorizationHandler), ServiceLifetime.Singleton);
+                services.Add(descriptor);
+            }
+            return services;
+        }
+    }
+}
---
+++ Sources/EasyExtensions.Authorization/Handlers/AllowAnonymousAuthorizationHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace EasyExtensions.Authorization.Handlers
+{
+    /// <summary>
+    /// This authorization handler will bypass all requirements
+    /// </summary>
+    public class AllowAnonymousAuthorizationHandler : IAuthorizationHandler
+    {
+        /// <summary>
+        /// <see cref="IAuthorizationHandler"/> contract.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public Task HandleAsync(AuthorizationHandlerContext context)
+        {
+            foreach (IAuthorizationRequirement requirement in context.PendingRequirements)
+            {
+                context.Succeed(requirement); //Simply pass all requirements
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
