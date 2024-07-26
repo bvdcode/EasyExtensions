e9795969c8735948bced639f627a95927ad83338
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-07-25 19:55:47 -0700

    Add Quartz project and job scheduling support
    Added a new project `EasyExtensions.Quartz` to the solution, targeting `netstandard2.1` and including necessary metadata and package references. Introduced `Microsoft.SourceLink.GitHub` package to `EasyExtensions.csproj` for source link support. Created `JobTriggerAttribute` for setting job trigger intervals and extended `IServiceCollection` with `AddQuartzJobs` method to configure Quartz jobs and triggers based on the attribute.

---
+++ Sources/EasyExtensions.Quartz/Attributes/JobTriggerAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Quartz;
+using System;
+
+namespace EasyExtensions.Quartz.Attributes
+{
+    /// <summary>
+    /// Add this attribute to the class inherited from <see cref="IJob"/> to set the trigger interval.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class JobTriggerAttribute : Attribute
+    {
+        /// <summary>
+        /// Job trigger interval.
+        /// </summary>
+        public TimeSpan Interval { get; }
+
+        /// <summary>
+        /// Create a new instance of <see cref="JobTriggerAttribute"/>.
+        /// </summary>
+        /// <param name="days"> Days. </param>
+        /// <param name="hours"> Hours. </param>
+        /// <param name="minutes"> Minutes. </param>
+        /// <param name="seconds"> Seconds. </param>
+        public JobTriggerAttribute(int days = 0, int hours = 0, int minutes = 0, int seconds = 0)
+        {
+            if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0)
+            {
+                throw new ArgumentException("At least one of the parameters must be greater than 0.");
+            }
+            Interval = new TimeSpan(days, hours, minutes, seconds);
+        }
+    }
+}
---
+++ Sources/EasyExtensions.Quartz/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Quartz;
+using System.Reflection;
+using EasyExtensions.Helpers;
+using EasyExtensions.Quartz.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasyExtensions.Quartz
+{
+    /// <summary>
+    /// <see cref="IServiceCollection"/> extensions.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds Quartz jobs with <see cref="JobTriggerAttribute"/> to the <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="services">IServiceCollection instance.</param>
+        /// <returns> Current <see cref="IServiceCollection"/> instance. </returns>
+        public static IServiceCollection AddQuartzJobs(this IServiceCollection services)
+        {
+            return services.AddQuartz(SetupQuartz).AddQuartzHostedService();
+        }
+
+        private static void SetupQuartz(IServiceCollectionQuartzConfigurator configurator)
+        {
+            var jobs = ReflectionHelpers.GetTypesOfInterface(nameof(IJob));
+            foreach (var job in jobs)
+            {
+                JobTriggerAttribute? jobTriggerAttribute = job.GetCustomAttribute<JobTriggerAttribute>();
+                if (jobTriggerAttribute == null)
+                {
+                    continue;
+                }
+                var jobKey = new JobKey(job.Name);
+                configurator.AddJob(job, jobKey, x => x.WithIdentity(jobKey));
+                configurator.AddTrigger(opts =>
+                {
+                    opts.ForJob(jobKey)
+                        .WithIdentity(job.Name + "Trigger")
+                        .WithSimpleSchedule(x => x
+                        .WithInterval(jobTriggerAttribute.Interval)
+                        .RepeatForever());
+                });
+            }
+        }
+    }
+}
