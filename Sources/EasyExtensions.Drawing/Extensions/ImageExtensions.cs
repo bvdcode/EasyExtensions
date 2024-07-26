ada879b782c5f4a82fec12e5e5a8e2c1b4e103a7
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-07-26 02:50:07 -0700

    Add EasyExtensions.Drawing project with image processing
    A new project named `EasyExtensions.Drawing` has been added to the solution `EasyExtensions.sln`. This project targets .NET 8.0 and includes metadata such as authorship, company, product description, and licensing information. It references a README file, a package icon, and a license file for packaging purposes.
    
    Dependencies include `SixLabors.ImageSharp.Drawing` version 2.1.3 and `Microsoft.SourceLink.GitHub` version 8.0.0. The project also references the existing `EasyExtensions` project. Build configurations for Debug and Release for Any CPU have been added.
    
    A new extension method `FitBluredBackground` has been introduced in the `EasyExtensions.Drawing.Extensions` namespace. This method resizes an image to fit a target size and applies a Gaussian blur to the background using the ImageSharp library.

---
+++ Sources/EasyExtensions.Drawing/Extensions/ImageExtensions.cs
@@ -0,0 +1,41 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace EasyExtensions.Drawing.Extensions
+{
+    /// <summary>
+    /// Image extensions.
+    /// </summary>
+    public static class ImageExtensions
+    {
+        /// <summary>
+        /// Fit image to target size and copy and blur it to background.
+        /// </summary>
+        /// <param name="image">Target image.</param>
+        /// <param name="targetWidth">Target width.</param>
+        /// <param name="targetHeight">Target height.</param>
+        /// <param name="gaussianBlurLevel">Gaussian blur level (optional).</param>
+        /// <returns cref="Image">Image with blured background.</returns>
+        public static Image FitBluredBackground(this Image image, int targetWidth, int targetHeight, float gaussianBlurLevel = 8F)
+        {
+            Image template = image.Clone(x => x.Resize(new ResizeOptions
+            {
+                Mode = ResizeMode.Crop,
+                Size = new Size(targetWidth, targetHeight)
+            }));
+            template.Mutate(x => x.GaussianBlur(gaussianBlurLevel));
+            using var cloned = image.Clone(x => x.Resize(new ResizeOptions
+            {
+                Mode = ResizeMode.Max,
+                Size = new Size(targetWidth, targetHeight)
+            }));
+
+            int x = template.Width > cloned.Width ? (template.Width - cloned.Width) / 2 : 0;
+            int y = template.Height > cloned.Height ? (template.Height - cloned.Height) / 2 : 0;
+
+            Point point = new(x, y);
+            template.Mutate(x => x.DrawImage(cloned, point, 1F));
+            return template;
+        }
+    }
+}
