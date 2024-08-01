847d39d4f4cc5625e253022f8593e9a95881cc97
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-08-01 15:46:18 -0700

    Make ITokenProvider public and add XML documentation
    The ITokenProvider interface has been changed from internal to public, allowing it to be accessed from other assemblies. Additionally, XML documentation comments have been added to the interface and its CreateToken method, including summaries and descriptions for the method's parameter and return value. This enhances code readability and provides better guidance for developers.

--- Sources/EasyExtensions.Authorization/Services/ITokenProvider.cs
+++ Sources/EasyExtensions.Authorization/Services/ITokenProvider.cs
@@ -3,8 +3,16 @@ using EasyExtensions.Authorization.Builders;
 
 namespace EasyExtensions.Authorization.Services
 {
-    internal interface ITokenProvider
+    /// <summary>
+    /// Provides token creation with claims.
+    /// </summary>
+    public interface ITokenProvider
     {
+        /// <summary>
+        /// Creates a token with claims.
+        /// </summary>
+        /// <param name="claimBuilder"> Optional claim builder. </param>
+        /// <returns> Token. </returns>
         string CreateToken(Func<ClaimBuilder, ClaimBuilder>? claimBuilder = null);
     }
 }
