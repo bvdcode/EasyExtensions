002b1fe756e09a76db7adcd7562dd881f0553f06
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-08-04 03:02:15 -0700

    Add Brotli compression support to HttpClient
    Introduced BrotliHttpClient and BrotliCompressionHandler classes.
    - BrotliHttpClient.cs:
      - Added necessary using directives.
      - Defined EasyExtensions.Clients namespace.
      - Created BrotliHttpClient class inheriting from HttpClient.
      - Implemented constructor initializing with BrotliCompressionHandler.
    - BrotliCompressionHandler.cs:
      - Added necessary using directives.
      - Defined EasyExtensions.Handlers namespace.
      - Created internal BrotliCompressionHandler class inheriting from HttpClientHandler.
      - Overrode SendAsync to handle Brotli-compressed responses.
        - Decompresses Brotli content using BrotliStream.
        - Copies decompressed content to new StreamContent.
        - Preserves original headers and replaces response content.

---
+++ Sources/EasyExtensions/Clients/BrotliHttpClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using EasyExtensions.Handlers;
+
+namespace EasyExtensions.Clients
+{
+    /// <summary>
+    /// BrotliHttpClient is a HttpClient that supports Brotli compression.
+    /// </summary>
+    public class BrotliHttpClient : HttpClient
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="BrotliHttpClient"/>.
+        /// </summary>
+        public BrotliHttpClient() : base(new BrotliCompressionHandler()) { }
+    }
+}
---
+++ Sources/EasyExtensions/Handlers/BrotliCompressionHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.IO.Compression;
+using System.Threading.Tasks;
+
+namespace EasyExtensions.Handlers
+{
+    internal class BrotliCompressionHandler : HttpClientHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = await base.SendAsync(request, cancellationToken);
+            if (!response.Content.Headers.TryGetValues("Content-Encoding", out var ce) || ce.First() != "br")
+            {
+                return response;
+            }
+            var inputStream = await response.Content.ReadAsStreamAsync();
+            using BrotliStream stream = new BrotliStream(inputStream, CompressionMode.Decompress);
+            MemoryStream outputStream = new MemoryStream();
+            await stream.CopyToAsync(outputStream);
+            outputStream.Seek(default, SeekOrigin.Begin);
+            var content = new StreamContent(outputStream);
+            foreach (var item in response.Content.Headers)
+            {
+                if (!content.Headers.Contains(item.Key))
+                {
+                    content.Headers.Add(item.Key, item.Value);
+                }
+            }
+            response.Content = content;
+            return response;
+        }
+    }
+}
