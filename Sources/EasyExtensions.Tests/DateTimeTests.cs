9ecb1987fff579262020dc6c5f71ecf4594bbf79
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-08-07 13:40:28 -0700

    Add Unix timestamp methods and tests to DateTimeExtensions
    Added ToUnixTimestampMilliseconds and ToUnixTimestampSeconds
    methods to DateTimeExtensions class, both converting DateTime
    to Unix timestamps and throwing ArgumentException if not UTC.
    Introduced DateTimeTests class in EasyExtensions.Tests to
    validate new and existing methods, including tests for
    DropMilliseconds and ToUniversalTimeWithoutOffset.

---
+++ Sources/EasyExtensions.Tests/DateTimeTests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyExtensions.Tests
+{
+    public class DateTimeTests
+    {
+        [Test]
+        public void ToUnixTimestampMilliseconds_ValidInput_ValidOutput()
+        {
+            var dateTime = new DateTime(2021, 1, 1, 0, 0, 52, 185, DateTimeKind.Utc);
+            Assert.That(dateTime.ToUnixTimestampMilliseconds(), Is.EqualTo(1609459252185));
+        }
+
+        [Test]
+        public void ToUnixTimestampSeconds_ValidInput_ValidOutput()
+        {
+            var dateTime = new DateTime(2024, 8, 7, 20, 34, 55, DateTimeKind.Utc);
+            Assert.That(dateTime.ToUnixTimestampSeconds(), Is.EqualTo(1723062895));
+        }
+
+        [Test]
+        public void DropMilliseconds_ValidInput_ValidOutput()
+        {
+            var dateTime = new DateTime(2021, 1, 1, 0, 0, 0, 123, DateTimeKind.Utc);
+            Assert.That(dateTime.DropMilliseconds(), Is.EqualTo(new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Utc)));
+        }
+
+        [Test]
+        public void DropMillisecondsOffset_ValidInput_ValidOutput()
+        {
+            var dateTimeOffset = new DateTimeOffset(2021, 1, 1, 0, 0, 0, 123, TimeSpan.Zero);
+            Assert.That(dateTimeOffset.DropMilliseconds(), Is.EqualTo(new DateTimeOffset(2021, 1, 1, 0, 0, 0, TimeSpan.Zero)));
+        }
+
+        [Test]
+        public void ToUniversalTimeWithoutOffset_ValidInput_ValidOutput()
+        {
+            var dateTime = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Local);
+            Assert.That(dateTime.ToUniversalTimeWithoutOffset(), Is.EqualTo(new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Utc)));
+        }
+
+        [Test]
+        public void ToUniversalTimeWithoutOffset_ValidInput_ValidKind()
+        {
+            var dateTime = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Local);
+            Assert.That(dateTime.ToUniversalTimeWithoutOffset().Kind, Is.EqualTo(DateTimeKind.Utc));
+        }
+
+        [Test]
+        public void ToUniversalTimeWithoutOffset_ValidInput_ValidTicks()
+        {
+            var dateTime = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Local);
+            Assert.That(dateTime.ToUniversalTimeWithoutOffset().Ticks, Is.EqualTo(new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks));
+        }
+
+        [Test]
+        public void ToUniversalTimeWithoutOffset_ValidInput_ValidKindOffset()
+        {
+            var dateTime = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Local);
+            Assert.That(dateTime.ToUniversalTimeWithoutOffset().Kind, Is.EqualTo(DateTimeKind.Utc));
+        }
+
+        [Test]
+        public void ToUniversalTimeWithoutOffset_ValidInput_ValidTicksOffset()
+        {
+            var dateTime = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Local);
+            Assert.That(dateTime.ToUniversalTimeWithoutOffset().Ticks, Is.EqualTo(new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks));
+        }
+
+        [Test]
+        public void ToUniversalTimeWithoutOffset_ValidInput_ValidKindUtc()
+        {
+            var dateTime = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            Assert.That(dateTime.ToUniversalTimeWithoutOffset().Kind, Is.EqualTo(DateTimeKind.Utc));
+        }
+
+        [Test]
+        public void ToUniversalTimeWithoutOffset_ValidInput_ValidTicksUtc()
+        {
+            var dateTime = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            Assert.That(dateTime.ToUniversalTimeWithoutOffset().Ticks, Is.EqualTo(new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks));
+        }
+
+        [Test]
+        public void ToUniversalTimeWithoutOffset_ValidInput_ValidKindUnspecified()
+        {
+            var dateTime = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Unspecified);
+            Assert.That(dateTime.ToUniversalTimeWithoutOffset().Kind, Is.EqualTo(DateTimeKind.Utc));
+        }
+
+        [Test]
+        public void ToUniversalTimeWithoutOffset_ValidInput_ValidTicksUnspecified()
+        {
+            var dateTime = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Unspecified);
+            Assert.That(dateTime.ToUniversalTimeWithoutOffset().Ticks, Is.EqualTo(new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks));
+        }
+
+        [Test]
+        public void ToUniversalTimeWithoutOffset_ValidInput_ValidKindLocal()
+        {
+            var dateTime = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Local);
+            Assert.That(dateTime.ToUniversalTimeWithoutOffset().Kind, Is.EqualTo(DateTimeKind.Utc));
+        }
+
+        [Test]
+        public void ToUniversalTimeWithoutOffset_ValidInput_ValidTicksLocal()
+        {
+            var dateTime = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Local);
+            Assert.That(dateTime.ToUniversalTimeWithoutOffset().Ticks, Is.EqualTo(new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks));
+        }
+
+        [Test]
+        public void ToUnixTimestampMilliseconds_InvalidInput_ThrowArgumentException()
+        {
+            var dateTime = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Local);
+            Assert.Throws(typeof(ArgumentException), () => dateTime.ToUnixTimestampMilliseconds());
+        }
+
+        [Test]
+        public void ToUnixTimestampSeconds_InvalidInput_ThrowArgumentException()
+        {
+            var dateTime = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Local);
+            Assert.Throws(typeof(ArgumentException), () => dateTime.ToUnixTimestampSeconds());
+        }
+    }
+}
--- Sources/EasyExtensions/Extensions/DateTimeExtensions.cs
+++ Sources/EasyExtensions/Extensions/DateTimeExtensions.cs
@@ -8,6 +8,31 @@ namespace EasyExtensions
     public static class DateTimeExtensions
     {
         /// <summary>
+        /// Convert <see cref="DateTime"/> to Unix timestamp in milliseconds.
+        /// </summary>
+        /// <returns> Unix timestamp in milliseconds. </returns>
+        /// <exception cref="ArgumentException"> DateTime value must be in UTC timezone. </exception>
+        public static long ToUnixTimestampMilliseconds(this DateTime value)
+        {
+            if (value.Kind != DateTimeKind.Utc)
+            {
+                throw new ArgumentException("DateTime value must be in UTC timezone.");
+            }
+            DateTimeOffset dateTimeOffset = new DateTimeOffset(value);
+            return dateTimeOffset.ToUnixTimeMilliseconds();
+        }
+
+        /// <summary>
+        /// Convert <see cref="DateTime"/> to Unix timestamp in seconds.
+        /// </summary>
+        /// <returns> Unix timestamp in seconds. </returns>
+        /// <exception cref="ArgumentException"> DateTime value must be in UTC timezone. </exception>
+        public static int ToUnixTimestampSeconds(this DateTime value)
+        {
+            return (int)(value.ToUnixTimestampMilliseconds() / 1000);
+        }
+
+        /// <summary>
         /// Remove milliseconds from <see cref="DateTime"/>.
         /// </summary>
         /// <returns> DateTime without milliseconds. </returns>
