6f455cc49f105f9163d69eb13d9b38791dd67401
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-08-09 03:29:58 -0700

    Add TriggerJobAsync<TJob> extension for ISchedulerFactory
    Introduced a new file `SchedulerFactoryExtensions.cs` with an
    asynchronous extension method `TriggerJobAsync<TJob>` for the
    `ISchedulerFactory` interface from the Quartz library. This
    method allows triggering a job of type `TJob` by its job key.

---
+++ Sources/EasyExtensions.Quartz/Extensions/SchedulerFactoryExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Quartz;
+using System.Threading.Tasks;
+
+namespace EasyExtensions.Quartz.Extensions
+{
+    /// <summary>
+    /// <see cref="ISchedulerFactory"/> extensions.
+    /// </summary>
+    public static class SchedulerFactoryExtensions
+    {
+        /// <summary>
+        /// Force triggers the job.
+        /// </summary>
+        /// <typeparam name="TJob">Job type.</typeparam>
+        /// <param name="schedulerFactory"><see cref="ISchedulerFactory"/> instance.</param>
+        /// <returns>Task representing the asynchronous operation.</returns>
+        public static async Task TriggerJobAsync<TJob>(this ISchedulerFactory schedulerFactory)
+            where TJob : IJob
+        {
+            JobKey jobKey = new JobKey(typeof(TJob).Name);
+            var scheduler = await schedulerFactory.GetScheduler();
+            await scheduler.TriggerJob(jobKey);
+        }
+    }
+}
