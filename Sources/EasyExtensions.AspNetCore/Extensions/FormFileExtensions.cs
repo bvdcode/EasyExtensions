8e239e9781a58a2f13fe4e036a881bbd1efc5073
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-10-15 09:47:36 -0700

    Add extension method to convert IFormFile to byte array
    Introduced a new file `FormFileExtensions.cs` containing an extension method for the `IFormFile` interface. This method, `ToBytes`, reads the file content and returns it as a byte array. The new functionality is encapsulated within the `EasyExtensions.AspNetCore.Extensions` namespace. Imported necessary namespaces: `System.IO` and `Microsoft.AspNetCore.Http`.

---
+++ Sources/EasyExtensions.AspNetCore/Extensions/FormFileExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace EasyExtensions.AspNetCore.Extensions
+{
+    /// <summary>
+    /// <see cref="IFormFile"/> extensions.
+    /// </summary>
+    public static class FormFileExtensions
+    {
+        /// <summary>
+        /// Reads the <see cref="IFormFile"/> as a byte array.
+        /// </summary>
+        /// <param name="file"> The <see cref="IFormFile"/>. </param>
+        /// <returns> The byte array. </returns>
+        public static byte[] ToBytes(this IFormFile file)
+        {
+            ArgumentNullException.ThrowIfNull(file);
+            using MemoryStream ms = new();
+            file.CopyTo(ms);
+            return ms.ToArray();
+        }
+    }
+}
