b3a8be122a30f80c2c2e040594810530143c8550
Vadim Belov <github@belov.us>
2024-12-08 07:57:45 -0800

    Add StopwatchDebugger class and corresponding tests
    Introduce StopwatchDebugger class in EasyExtensions.Debuggers to measure and report elapsed time via a callback function. Include methods to start, report, and restart the stopwatch, and a nested class to encapsulate action name and elapsed time. Add StopwatchDebuggerTests class in EasyExtensions.Tests to verify the functionality of StopwatchDebugger, ensuring it correctly measures and reports elapsed time.

---
+++ Sources/EasyExtensions.Tests/StopwatchDebuggerTests.cs
@@ -0,0 +1,33 @@
+﻿using EasyExtensions.Debuggers;
+
+namespace EasyExtensions.Tests
+{
+    internal class StopwatchDebuggerTests
+    {
+        readonly StopwatchDebugger debugger;
+        string action = string.Empty;
+        TimeSpan elapsed = TimeSpan.Zero;
+
+        public StopwatchDebuggerTests()
+        {
+            debugger = new StopwatchDebugger(x =>
+            {
+                action = x.Action;
+                elapsed = x.Elapsed;
+            });
+        }
+
+        [Test]
+        public void CreateDebugger_ValidInput_ValidOutput()
+        {
+            debugger.Start();
+            Task.Delay(1000).Wait();
+            debugger.Report("Started");
+            Assert.Multiple(() =>
+            {
+                Assert.That(action, Is.EqualTo("Started"));
+                Assert.That(elapsed.TotalSeconds, Is.GreaterThan(0.9).And.LessThan(1.1));
+            });
+        }
+    }
+}
---
+++ Sources/EasyExtensions/Debuggers/StopwatchDebugger.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace EasyExtensions.Debuggers
+{
+    /// <summary>
+    /// StopwatchDebugger class to measure elapsed time.
+    /// </summary>
+    public class StopwatchDebugger
+    {
+        private readonly Stopwatch _stopwatch;
+        private readonly Action<StopwatchDebuggerEventArgs> _callback;
+
+        /// <summary>
+        /// Create a new instance of StopwatchDebugger.
+        /// </summary>
+        /// <param name="callback">Callback function to report elapsed time.</param>
+        public StopwatchDebugger(Action<StopwatchDebuggerEventArgs> callback)
+        {
+            _callback = callback ?? throw new ArgumentNullException(nameof(callback));
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        /// <summary>
+        /// Report the elapsed time.
+        /// </summary>
+        /// <param name="action">Action name.</param>
+        /// <exception cref="ArgumentNullException">Thrown when action is null or empty.</exception>
+        public void Report(string action)
+        {
+            if (string.IsNullOrWhiteSpace(action))
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+            _stopwatch.Stop();
+            _callback(new StopwatchDebuggerEventArgs
+            {
+                Action = action,
+                Elapsed = _stopwatch.Elapsed
+            });
+            _stopwatch.Restart();
+        }
+
+        /// <summary>
+        /// Start the stopwatch.
+        /// </summary>
+        public void Start()
+        {
+            _stopwatch.Restart();
+        }
+
+        /// <summary>
+        /// StopwatchDebuggerEventArgs class to report elapsed time.
+        /// </summary>
+        public class StopwatchDebuggerEventArgs : EventArgs
+        {
+            /// <summary>
+            /// Action name.
+            /// </summary>
+            public string Action { get; set; } = string.Empty;
+
+            /// <summary>
+            /// Elapsed time.
+            /// </summary>
+            public TimeSpan Elapsed { get; set; }
+        }
+    }
+}
