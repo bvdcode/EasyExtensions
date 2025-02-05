51ccc9267f0f3aacd413860a353bcd1e53010816
Vadim Belov <github@belov.us>
2025-02-05 04:34:43 -0800

    Add DesignTimeDbContextFactory for design-time DbContext creation
    Introduce DesignTimeDbContextFactory<TContext> in EasyExtensions.EntityFrameworkCore.Npgsql.Migrations namespace.
    Implement IDesignTimeDbContextFactory<TContext> interface.
    Use IServiceScopeFactory for dependency injection.
    Add necessary using directives.

---
+++ Sources/EasyExtensions.EntityFrameworkCore.Npgsql/Migrations/DesignTimeDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasyExtensions.EntityFrameworkCore.Npgsql.Migrations
+{
+    /// <summary>
+    /// This class is used to create a new instance of the <see cref="DbContext"/> with the specified arguments.
+    /// Usually used in design time services, such as migrations.
+    /// </summary>
+    /// <typeparam name="TContext"> The type of <see cref="DbContext"/> to create. </typeparam>
+    /// <param name="_serviceScopeFactory"> The <see cref="IServiceScopeFactory"/> instance. </param>
+    public class DesignTimeDbContextFactory<TContext>(IServiceScopeFactory _serviceScopeFactory)
+        : IDesignTimeDbContextFactory<TContext> where TContext : DbContext
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="DbContext"/> with the specified arguments.
+        /// </summary>
+        /// <param name="args"> The arguments to use when creating the <see cref="DbContext"/>. </param>
+        /// <returns> A new instance of the <see cref="DbContext"/>. </returns>
+        public TContext CreateDbContext(string[] args)
+        {
+            var serviceProvider = _serviceScopeFactory.CreateScope().ServiceProvider;
+            return serviceProvider.GetRequiredService<TContext>();
+        }
+    }
+}
