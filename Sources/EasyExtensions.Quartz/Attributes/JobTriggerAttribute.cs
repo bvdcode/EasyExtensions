d72f863866c1f18556eb88efab52cece2682fb74
Vadim Belov <github@belov.us>
2025-02-07 21:59:50 -0800

    Add optional startAt parameter to JobTriggerAttribute
    The `JobTriggerAttribute` constructor in the `EasyExtensions.Quartz.Attributes`
    namespace now includes a new optional parameter `startAt` of type `TimeOfDay?`,
    allowing specification of a specific time of day to start the job. The order of
    initialization in the constructor has been adjusted to set the `StartAt` property
    before the `Interval` property. The XML documentation comment for the constructor
    has been updated to include a description for the new `startAt` parameter.

--- Sources/EasyExtensions.Quartz/Attributes/JobTriggerAttribute.cs
+++ Sources/EasyExtensions.Quartz/Attributes/JobTriggerAttribute.cs
@@ -17,7 +17,7 @@ namespace EasyExtensions.Quartz.Attributes
         /// <summary>
         /// Start the job at the specified time.
         /// </summary>
-        public TimeOfDay? StartAt { get; set; }
+        public TimeOfDay? StartAt { get; }
 
         /// <summary>
         /// Start the job immediately.
@@ -27,7 +27,7 @@ namespace EasyExtensions.Quartz.Attributes
         /// <summary>
         /// Repeat the job forever.
         /// </summary>
-        public bool RepeatForever { get; set; }
+        public bool RepeatForever { get; }
 
         /// <summary>
         /// Create a new instance of <see cref="JobTriggerAttribute"/>.
@@ -38,16 +38,18 @@ namespace EasyExtensions.Quartz.Attributes
         /// <param name="seconds"> Seconds. </param>
         /// <param name="startNow"> Start now. </param>
         /// <param name="repeatForever"> Repeat forever. </param>
+        /// <param name="startAt"> Start at. </param>
         public JobTriggerAttribute(int days = 0, int hours = 0, int minutes = 0, int seconds = 0,
-            bool startNow = true, bool repeatForever = true)
+            bool startNow = true, bool repeatForever = true, TimeOfDay? startAt = null)
         {
             if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0)
             {
                 throw new ArgumentException("At least one of the parameters must be greater than 0.");
             }
-            Interval = new TimeSpan(days, hours, minutes, seconds);
+            StartAt = startAt;
             StartNow = startNow;
             RepeatForever = repeatForever;
+            Interval = new TimeSpan(days, hours, minutes, seconds);
         }
     }
 }
