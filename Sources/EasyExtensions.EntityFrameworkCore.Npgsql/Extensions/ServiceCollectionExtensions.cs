e347b6fdb236fb9db0bc68c7aa4ae77ace7879c4
Vadim Belov <github@belov.us>
2025-03-19 16:18:12 -0700

    Refactor PostgreSQL DbContext configuration
    Moved `PostgresContextFactory` to a new file for better organization. Updated `ServiceCollectionExtensions.cs` to mark `AddPostgresDbContext<TContext>` as obsolete and introduced a new method that utilizes the `PostgresContextFactory`. Added necessary using directives to reflect the new structure.

--- Sources/EasyExtensions.EntityFrameworkCore.Npgsql/Extensions/ServiceCollectionExtensions.cs
+++ Sources/EasyExtensions.EntityFrameworkCore.Npgsql/Extensions/ServiceCollectionExtensions.cs
@@ -4,55 +4,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.DependencyInjection;
 using EasyExtensions.EntityFrameworkCore.Npgsql.Migrations;
+using EasyExtensions.EntityFrameworkCore.Npgsql.Factories;
 
 namespace EasyExtensions.EntityFrameworkCore.Npgsql.Extensions
 {
-    /// <summary>
-    /// This class is used to set up the <see cref="DbContext"/> for PostgreSQL with the specified arguments.
-    /// </summary>
-    public class PostgresContextFactory
-    {
-        /// <summary>
-        /// The maximum pool size for the database connection, default is 100.
-        /// </summary>
-        public int MaxPoolSize { get; set; } = 100;
-
-        /// <summary>
-        /// The connection timeout in seconds, default is 60 seconds.
-        /// </summary>
-        public int TimeoutSeconds { get; set; } = 60;
-
-        /// <summary>
-        /// The lifetime of the <see cref="DbContext"/>, default is <see cref="ServiceLifetime.Transient"/>.
-        /// </summary>
-        public ServiceLifetime ContextLifetime { get; set; } = ServiceLifetime.Transient;
-
-        /// <summary>
-        /// The timezone to use for the database connection, default is "UTC".
-        /// </summary>
-        public string Timezone { get; set; } = "UTC";
-
-        /// <summary>
-        /// The encoding to use for the database connection, default is "UTF8".
-        /// </summary>
-        public string Encoding { get; set; } = "UTF8";
-
-        /// <summary>
-        /// The section name in the <see cref="IConfiguration"/> to use for the database settings, default is "DatabaseSettings".
-        /// </summary>
-        public string ConfigurationSection { get; set; } = "DatabaseSettings";
-
-        /// <summary>
-        /// Whether to add the <see cref="IDesignTimeDbContextFactory{TContext}"/> to the service collection.
-        /// </summary>
-        public bool AddDesignTimeDbContextFactory { get; set; } = true;
-
-        /// <summary>
-        /// Setup action for the <see cref="NpgsqlConnectionStringBuilder"/> to customize the connection string.
-        /// </summary>
-        public Action<NpgsqlConnectionStringBuilder>? SetupConnectionString { get; set; }
-    }
-
     /// <summary>
     /// <see cref="IServiceCollection"/> extensions.
     /// </summary>
@@ -85,37 +40,6 @@ namespace EasyExtensions.EntityFrameworkCore.Npgsql.Extensions
                 services.AddScoped<IDesignTimeDbContextFactory<TContext>, DesignTimeDbContextFactory<TContext>>();
             }
             return services;
-        }
-
-
-        /// <summary>
-        /// Adds a <see cref="DbContext"/> to the <see cref="IServiceCollection"/> using the 
-        /// <see cref="IConfiguration"/> to build the connection string from DatabaseSettings section.
-        /// </summary>
-        /// <typeparam name="TContext"> The type of <see cref="DbContext"/> to add. </typeparam>
-        /// <param name="services"> The <see cref="IServiceCollection"/> instance. </param>
-        /// <param name="configuration"> The <see cref="IConfiguration"/> instance. </param>
-        /// <param name="maxPoolSize"> The maximum pool size, default is 100. </param>
-        /// <param name="timeoutSeconds"> The connection timeout in seconds, default is 60. </param>
-        /// <param name="contextLifetime"> The <see cref="ServiceLifetime"/> of the <see cref="DbContext"/>, default is Transient. </param>
-        /// <param name="setupConnectionString"> The action to setup the connection string builder. </param>
-        /// <param name="addDesignTimeDbContextFactory"> Whether to add the <see cref="IDesignTimeDbContextFactory{TContext}"/> to the service collection. </param>
-        /// <returns> Current <see cref="IServiceCollection"/> instance. </returns>
-        /// <exception cref="KeyNotFoundException"> When DatabaseSettings section is not set. </exception>
-        [Obsolete("Use AddPostgresDbContext<TContext>(IServiceCollection, IConfiguration, Action<PostgresContextFactory>) instead. This method will be removed in a future version.", false)]
-        public static IServiceCollection AddPostgresDbContext<TContext>(this IServiceCollection services,
-            IConfiguration configuration, int maxPoolSize = 100, int timeoutSeconds = 60, ServiceLifetime contextLifetime = ServiceLifetime.Transient,
-            Action<NpgsqlConnectionStringBuilder>? setupConnectionString = null, bool addDesignTimeDbContextFactory = false)
-            where TContext : DbContext
-        {
-            return AddPostgresDbContext<TContext>(services, configuration, x =>
-            {
-                x.MaxPoolSize = maxPoolSize;
-                x.TimeoutSeconds = timeoutSeconds;
-                x.ContextLifetime = contextLifetime;
-                x.SetupConnectionString = setupConnectionString;
-                x.AddDesignTimeDbContextFactory = addDesignTimeDbContextFactory;
-            });
         }
 
         private static string BuildConnectionString(IConfiguration configuration, PostgresContextFactory contextFactory)
---
+++ Sources/EasyExtensions.EntityFrameworkCore.Npgsql/Factories/PostgresContextFactory.cs
@@ -0,0 +1,54 @@
+﻿using Npgsql;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasyExtensions.EntityFrameworkCore.Npgsql.Factories
+{
+    /// <summary>
+    /// This class is used to set up the <see cref="DbContext"/> for PostgreSQL with the specified arguments.
+    /// </summary>
+    public class PostgresContextFactory
+    {
+        /// <summary>
+        /// The maximum pool size for the database connection, default is 100.
+        /// </summary>
+        public int MaxPoolSize { get; set; } = 100;
+
+        /// <summary>
+        /// The connection timeout in seconds, default is 60 seconds.
+        /// </summary>
+        public int TimeoutSeconds { get; set; } = 60;
+
+        /// <summary>
+        /// The lifetime of the <see cref="DbContext"/>, default is <see cref="ServiceLifetime.Transient"/>.
+        /// </summary>
+        public ServiceLifetime ContextLifetime { get; set; } = ServiceLifetime.Transient;
+
+        /// <summary>
+        /// The timezone to use for the database connection, default is "UTC".
+        /// </summary>
+        public string Timezone { get; set; } = "UTC";
+
+        /// <summary>
+        /// The encoding to use for the database connection, default is "UTF8".
+        /// </summary>
+        public string Encoding { get; set; } = "UTF8";
+
+        /// <summary>
+        /// The section name in the <see cref="IConfiguration"/> to use for the database settings, default is "DatabaseSettings".
+        /// </summary>
+        public string ConfigurationSection { get; set; } = "DatabaseSettings";
+
+        /// <summary>
+        /// Whether to add the <see cref="IDesignTimeDbContextFactory{TContext}"/> to the service collection.
+        /// </summary>
+        public bool AddDesignTimeDbContextFactory { get; set; } = true;
+
+        /// <summary>
+        /// Setup action for the <see cref="NpgsqlConnectionStringBuilder"/> to customize the connection string.
+        /// </summary>
+        public Action<NpgsqlConnectionStringBuilder>? SetupConnectionString { get; set; }
+    }
+}
