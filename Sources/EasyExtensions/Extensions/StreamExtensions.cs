291719066e9d1af657c517d952b95dbe44adf938
Vadim Belov <bvdcode@users.noreply.github.com>
2025-08-08 21:51:25 -0700

    Add leaveOpen parameter to ReadToEnd methods
    Updated the `ReadToEnd` and `ReadToEndAsync` methods in the `StreamExtensions` class to include a `leaveOpen` boolean parameter. This allows the caller to control whether the stream remains open after reading. If set to `false`, the stream will be disposed. XML documentation comments have been added to describe the new parameter.

--- Sources/EasyExtensions/Extensions/StreamExtensions.cs
+++ Sources/EasyExtensions/Extensions/StreamExtensions.cs
@@ -14,29 +14,41 @@ namespace EasyExtensions
         /// <summary>
         /// Reads the bytes from the current stream and writes them to the byte array.
         /// </summary>
+        /// <param name="stream"> The stream to read from. </param>
+        /// <param name="leaveOpen"> If true, the stream will not be disposed after reading. </param>
         /// <returns> Received byte array. </returns>
         /// <exception cref="IOException"> An I/O error occurred. </exception>
         /// <exception cref="ArgumentNullException"> Destination is null. </exception>
         /// <exception cref="ObjectDisposedException"> Either the current stream or the destination stream is disposed. </exception>
         /// <exception cref="NotSupportedException"> The current stream does not support reading, or the destination stream does not support writing. </exception>
-        public static byte[] ReadToEnd(this Stream stream)
+        public static byte[] ReadToEnd(this Stream stream, bool leaveOpen = false)
         {
             using MemoryStream ms = new MemoryStream();
             stream.CopyTo(ms);
+            if (!leaveOpen)
+            {
+                stream.Dispose();
+            }
             return ms.ToArray();
         }
 
         /// <summary>
         /// Asynchronously reads the bytes from the current stream and writes them to the byte array.
         /// </summary>
+        /// <param name="stream"> The stream to read from. </param>
+        /// <param name="leaveOpen"> If true, the stream will not be disposed after reading. </param>
         /// <returns> Received byte array. </returns>
         /// <exception cref="ArgumentNullException"> Destination is null. </exception>
         /// <exception cref="ObjectDisposedException"> Either the current stream or the destination stream is disposed. </exception>
         /// <exception cref="NotSupportedException"> The current stream does not support reading, or the destination stream does not support writing. </exception>
-        public static async Task<byte[]> ReadToEndAsync(this Stream stream)
+        public static async Task<byte[]> ReadToEndAsync(this Stream stream, bool leaveOpen = false)
         {
             using MemoryStream ms = new MemoryStream();
             await stream.CopyToAsync(ms);
+            if (!leaveOpen)
+            {
+                await stream.DisposeAsync();
+            }
             return ms.ToArray();
         }
 
