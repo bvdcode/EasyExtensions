087a974336c502cfc06957f3c31fe5e9e63ee83c
Vadim Belov <git@belov.us>
2025-12-09 09:25:53 -0800

    Move enums to shared namespace; add ExtendedRefreshToken
    Refactored AuthRejectionType and AuthType enums to EasyExtensions.Models.Enums for better reuse. Introduced ExtendedRefreshToken entity with additional metadata fields (IP address, user agent, location, device) and EF Core mapping for enhanced refresh token tracking.

---
+++ Sources/EasyExtensions.EntityFrameworkCore/Database/ExtendedRefreshToken.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using EasyExtensions.Models.Enums;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EasyExtensions.EntityFrameworkCore.Database
+{
+    [Table("refresh_tokens")]
+    [Index(nameof(Token), IsUnique = true)]
+    internal class ExtendedRefreshToken : RefreshToken
+    {
+        [Column("ip_address")]
+        public IPAddress IpAddress { get; set; } = null!;
+
+        [Column("user_agent")]
+        public string UserAgent { get; set; } = null!;
+
+        [Column("auth_type")]
+        public AuthType AuthType { get; set; }
+
+        [Column("country")]
+        public string? Country { get; set; }
+
+        [Column("region")]
+        public string? Region { get; set; }
+
+        [Column("city")]
+        public string? City { get; set; }
+
+        [Column("device")]
+        public string? Device { get; set; }
+    }
+}
